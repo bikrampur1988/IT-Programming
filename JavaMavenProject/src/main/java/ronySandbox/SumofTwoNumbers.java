c6c3d8bb920b88589be84f09292e75d2e219cc95
Amin <namin6175@gmail.com>
2022-03-18 22:33:48 -0400

    Java shared project added

---
+++ JavaMavenProject/src/main/java/ronySandbox/SumofTwoNumbers.java
@@ -0,0 +1,21 @@
+package ronySandbox;
+
+public class SumofTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int a = 1200;
+	int b = 2400;
+	int add= (a+b);
+	
+	int add1 = (a+b);
+	int add2 = (a+b);
+	System.out.println("The sum of the two number is:" + add);
+		
+	System.out.println("The sum of the two number is:" + add1);	
+	
+	System.out.println("The sum of the two number is:" + add2);	
+	}
+
+}
